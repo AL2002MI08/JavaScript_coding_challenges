ccd7d335ce94de7ff4a707327ee5e5d2ec26659d
AL2002MI08 <alexandramizero@gmail.com>
2024-05-27 17:59:53 +0200

    chore: function that use template literals

---
+++ calculatearea.js
@@ -0,0 +1,6 @@
+// Write a concise arrow function calculateArea(width, height) that returns the product of width and height.
+const calculateArea = (width, height) => width*height
+console.log(calculateArea(4,5))
+const evendd = n => (n%2 === 0) 
+const odd = n => (n%2 !==0)
+console.log(odd(5))
---
+++ templateliteral.js
@@ -0,0 +1,3 @@
+// Write a function greetUser(name) that uses template literals to create a personalized greeting message like "Welcome back, ${name}!".
+const greetUser = name => `Welcome back, ${name}`
+console.log(greetUser('Caleb'))
