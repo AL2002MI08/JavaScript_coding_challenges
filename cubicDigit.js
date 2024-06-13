d06a6a9c3e073f95d69f28518a5711db1605bc6f
AL2002MI08 <alexandramizero@gmail.com>
2024-06-13 12:29:33 +0200

    function  to return cubic numbers in a string

---
+++ cubicDigit.js
@@ -0,0 +1,7 @@
+function isSumOfCubes(s){
+    let arr=s.match(/\d{1,3}/g).filter(a=> {
+      for(var i=0, num = 0;i<a.length;i++)num+=Math.pow(a[i],3);
+      return num === +a;
+    });
+    return (!arr||arr.length===0) ? "Unlucky" : arr.map(Number).join(' ')+' '+arr.reduce((s,v)=>s+v*1,0)+' Lucky';
+  }
