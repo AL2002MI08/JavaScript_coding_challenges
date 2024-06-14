52bb96f07d9479519cfa8bbb7fe900f57c7234b3
AL2002MI08 <alexandramizero@gmail.com>
2024-06-14 11:42:02 +0200

    score of five six-sided dice

---
+++ greedIsGood.js
@@ -0,0 +1,9 @@
+function score(dice) {
+    let dc = [0,0,0,0,0,0], 
+        tdr = [1000,200,300,400,500,600], 
+        sdr = [100,0,0,0,50,0];
+    dice.forEach(function(x){ dc[x-1]++; });
+    return dc.reduce(function(s,x,i){ 
+      return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
+    },0);
+}
