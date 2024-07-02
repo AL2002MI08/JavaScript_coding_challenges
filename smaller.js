7b995a380049cee011b404029f13b50f619d4fcb
AL2002MI08 <alexandramizero@gmail.com>
2024-07-02 17:35:25 +0200

    function that returns an array containing at each index i

--- smaller.js
+++ smaller.js
@@ -1,3 +1,4 @@
+// Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
 function smaller(nums) {
     return nums.map((n, i) => {
       return nums.slice(i).filter(v => v < n).length
