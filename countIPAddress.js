be127293160b30e6ede44a5c905b571942d5152c
AL2002MI08 <alexandramizero@gmail.com>
2024-07-09 14:46:51 +0200

    function to convert IP address

--- countIPAddress.js
+++ countIPAddress.js
@@ -1,3 +1,4 @@
+//function that takes to IP addresses and returns the number of addresses btn them
 function ipsBetween(start, end) {
     function val(ip) {
       return ip.split('.').reduce(function(tot, cur, i) {
