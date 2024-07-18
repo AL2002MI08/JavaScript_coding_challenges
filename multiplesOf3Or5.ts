2bb8c1ab2c9d07bda71cbaa8c3ecfbc0184863f4
AL2002MI08 <alexandramizero@gmail.com>
2024-07-18 11:23:55 +0200

    convert number to strung in typeScript

--- multiplesOf3Or5.ts
+++ multiplesOf3Or5.ts
@@ -1,7 +1,7 @@
 export class Challenge {
     static solution(number: number) {
      if(number <0) return 0;
-      let sum = 0;
+      let sum:number = 0;
       for(let i:number =0; i < number; i++){
         if(i% 3 === 0 || i% 5===0 )
           sum +=i
